8a757d229ab4cf11be5bd2c3564f73e4107714a5
FedericoQuartieri <federico.quartieri2002@gmail.com>
2024-11-16 19:28:25 +0100

    added error handling in scripts

--- LinuxInstallDependencies.sh
+++ LinuxInstallDependencies.sh
@@ -2,5 +2,9 @@
 
 sudo apt install python3.10-venv
 python3 -m venv env
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create virtual environment."
+    exit 1
+fi
 source env/bin/activate
 python3 -m pip install -r requirements.txt
--- MacInstallDependencies.sh
+++ MacInstallDependencies.sh
@@ -1,5 +1,9 @@
 #[bash]
 
 python3 -m venv env
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create virtual environment."
+    exit 1
+fi
 source env/bin/activate
 python3 -m pip install -r requirements.txt
